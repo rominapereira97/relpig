9f87024d3229ae80fe97009717007aeb01e93503
rominapereira <rominapereira97@gmail.com>
2019-07-24 14:51:10 -0300

    vidas

--- relpig.py
+++ relpig.py
@@ -83,6 +83,8 @@ class MyGame(arcade.Window):
     def setup(self, level):
         """ Set up the game here. Call this function to restart the game. """
 
+        self.wuinner = arcade.load_texture("images/winner.jpg")
+
         # Used to keep track of our scrolling
         self.view_bottom = 0
         self.view_left = 0
@@ -256,6 +258,11 @@ class MyGame(arcade.Window):
 
         # Clear the screen to the background color
         arcade.start_render()
+
+    if self.level==7:
+       arcade.draw_texture_winner(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
+                                     SCREEN_WIDTH, SCREEN_HEIGHT, self.winner)
+
 
         # Draw our sprites
         self.wall_list.draw()
